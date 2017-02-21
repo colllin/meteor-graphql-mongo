5f5b7a4faa4efa9f9108b59b5a715533f0ad2d2a
Collin Donahue-Oponski <collindo@gmail.com>
2017-02-20 21:55:02 -0700

    package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'colllin:graphql-mongo',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'GraphQL subscriptions for Mongo Collections.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/colllin/meteor-graphql-mongo',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
